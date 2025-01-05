aceea81d7e03a682be9ad1d04164b5d9bfe0bb29
Admin istrator <mouad.handaji@protonmail.com>
2025-01-05 18:49:33 +0100

    added payment modal

--- Client/src/pages/reservation/Reservation.jsx
+++ Client/src/pages/reservation/Reservation.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect,useState, useContext } from 'react'
+import React, { useEffect,useState, useContext, useRef } from 'react'
 import { useParams } from 'react-router-dom'
 import { useNavigate } from "react-router";
 
@@ -14,10 +14,10 @@ const Reservation = () => {
     const {userId} = useContext(AuthContext)
     const [nights, setNights] = useState(1);
     const [price, setPrice] = useState(0);
-    
-    
     const [listing, setListing] = useState([])
     const [loading, setLoading] = useState(true)
+    const [paymentOpened, setPaymentOpened] = useState(false)
+    const paymentRef = useRef()
     let navigate = useNavigate();
 
     useEffect(()=>{
@@ -30,18 +30,9 @@ const Reservation = () => {
       }
       )
     }, [])
-    
-  function handleChange(e){
-    if (e.target.value>=1){
-      setNights(e.target.value);
-    setPrice(listing.price*e.target.value);
-    }
-    
-  }
-  async function handleClick() {
-    if (!userId) {
-      navigate('/login')
-    }
+
+    async function handleClick() {
+      
     const data = {
       nights,
       price,
@@ -57,6 +48,14 @@ const Reservation = () => {
         navigate(-1)
       }
     }
+    
+  function handleChange(e){
+    if (e.target.value>=1){
+      setNights(e.target.value);
+    setPrice(listing.price*e.target.value);
+    }
+    
+  }
   function available(listing){
     
     if (listing.bookings.length>0){
@@ -97,10 +96,37 @@ const Reservation = () => {
                 <p id='price'>{price} MAD</p>
               </div>
               
-              <button className={!available(listing)?'reserve-btn not-available-btn':'available-btn'} onClick={handleClick} disabled={available(listing)?false:true}>Reserve</button>
+              <button className={!available(listing)?'reserve-btn not-available-btn':'available-btn'} onClick={()=>{
+                if (!userId){
+                  navigate('/login');
+                  return
+                }
+                setPaymentOpened(true);
+
+              }} disabled={available(listing)?false:true}>Reserve</button>
           </div>
         </div>
-        
+        {paymentOpened&& 
+        <div className="payment-modal-overlay" onClick={(e)=>{
+          if(!paymentRef.current.contains(e.target))
+          setPaymentOpened(false);
+          }}>
+          <div ref={paymentRef} className="payment-modal">
+            <p className='confirm'>Confirm Payment</p>
+            <div className="pay">
+              <div className='payment-info'>
+                <p>Price: <span className='payment-price'>{price} MAD</span> for {nights} Nights.</p>
+                <div className='payment-icons'>
+                  <img className="icon" src="/src/assets/paypal.png" title="visa icons" style={{ width: `25px`, height: `25px` }}/>
+                  <img className="icon" src="/src/assets/visa.png" title="visa icons" style={{ width: `40px`, height: `40px` }} />
+                  <img className="icon" src="/src/assets/apple-pay.png" title="visa icons" style={{ width: `50px`, height: `50px` }} />
+                </div>
+              </div>
+              <button onClick={handleClick}>Accept</button>
+            </div>
+            
+          </div>
+        </div>}
        
       </div>
     :
